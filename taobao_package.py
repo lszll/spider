bccd81cb66e4facc0bf717275b0868e0ac101ed5
lszll <lsz.ll@qq.com>
2017-07-27 14:15:15 +0800

    v0.1.1 - 修改prince -> price

--- taobao_package.py
+++ taobao_package.py
@@ -7,29 +7,27 @@ Created on Thu Jul 27 10:59:57 2017
 
 import requests
 import re
-
+ 
 def getHTMLText(url):
     try:
-        r = requests.get(url, timeout = 30)
+        r = requests.get(url, timeout=30)
         r.raise_for_status()
         r.encoding = r.apparent_encoding
         return r.text
     except:
-        return ""        
-
-    
+        return ""
+     
 def parsePage(ilt, html):
     try:
-        plt = re.findall(r'\"view_prince\"\:\"[\d\.]*\"', html)  #正则搜索的结果为 “view_price” : "{d.}"
-        tlt = re.findall(r'\"raw_title\"\:\:.*?\"', html)
+        plt = re.findall(r'\"view_price\"\:\"[\d\.]*\"',html)
+        tlt = re.findall(r'\"raw_title\"\:\".*?\"',html)
         for i in range(len(plt)):
             price = eval(plt[i].split(':')[1])
             title = eval(tlt[i].split(':')[1])
-            ilt.append([price, title])
+            ilt.append([price , title])
     except:
         print("")
-        
-
+ 
 def printGoodsList(ilt):
     tplt = "{:4}\t{:8}\t{:16}"
     print(tplt.format("序号", "价格", "商品名称"))
@@ -37,21 +35,20 @@ def printGoodsList(ilt):
     for g in ilt:
         count = count + 1
         print(tplt.format(count, g[0], g[1]))
-
-
+         
 def main():
-    goods = "书包"
+    goods = '书包'
     depth = 3
-    start_url = 'http://s.taobao.com/serach?q=' + goods
+    start_url = 'https://s.taobao.com/search?q=' + goods
     infoList = []
     for i in range(depth):
         try:
-            url = start_url + '&s=' + str(44*i)  #每页44个数据
+            url = start_url + '&s=' + str(44*i)
             html = getHTMLText(url)
             parsePage(infoList, html)
         except:
             continue
-    printGoodsList(infoList) 
+    printGoodsList(infoList)
   
 
 
