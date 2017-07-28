d96622b1ffed4962e1feb479daf0ec601910458b
lszll <lsz.ll@qq.com>
2017-07-28 13:43:12 +0800

    no message

--- sharesPull.py
+++ sharesPull.py
@@ -10,6 +10,11 @@ import bs4
 import re
 import traceback
 
+import requests
+from bs4 import BeautifulSoup
+import traceback
+import re
+ 
 def getHTMLText(url, code="utf-8"):
     try:
         r = requests.get(url)
@@ -18,50 +23,56 @@ def getHTMLText(url, code="utf-8"):
         return r.text
     except:
         return ""
-    
-
+ 
 def getStockList(lst, stockURL):
     html = getHTMLText(stockURL, "GB2312")
-    soup = BeautifulSoup(html, "html.parser")
+    soup = BeautifulSoup(html, 'html.parser') 
     a = soup.find_all('a')
     for i in a:
         try:
-            herf = i.attrs('href')
-            lst.append(re.findall(r"[s][hz]\d{6}", herf)[0])
+            href = i.attrs['href']
+            lst.append(re.findall(r"[s][hz]\d{6}", href)[0])
         except:
             continue
-
+ 
 def getStockInfo(lst, stockURL, fpath):
+    count = 0
     for stock in lst:
         url = stockURL + stock + ".html"
-        html = getHMTLText(url)
+        html = getHTMLText(url)
         try:
-            if html == "":
+            if html=="":
                 continue
             infoDict = {}
-            soup = BeautifulSoup(url, "html.parser")
-            stockInfo = soup.find('div', attrs={'class':'stock-bets'})
-            
-            name = stockInfo.fiind_all(attrs={'class':'bet-name'})[0]
+            soup = BeautifulSoup(html, 'html.parser')
+            stockInfo = soup.find('div',attrs={'class':'stock-bets'})
+ 
+            name = stockInfo.find_all(attrs={'class':'bets-name'})[0]
             infoDict.update({'股票名称': name.text.split()[0]})
-            
+             
             keyList = stockInfo.find_all('dt')
             valueList = stockInfo.find_all('dd')
             for i in range(len(keyList)):
                 key = keyList[i].text
                 val = valueList[i].text
                 infoDict[key] = val
-                
-            with open(fpath, 'a', encoding='utf-8' as f):
-                f.write(str(infoDict)+ '\n')
+             
+            with open(fpath, 'a', encoding='utf-8') as f:
+                f.write( str(infoDict) + '\n' )
                 count = count + 1
-                print('\r当前进度 : {:.2f}%', format(count*100/len(lst)),end='')
+                print("\r当前进度: {:.2f}%".format(count*100/len(lst)),end="")
         except:
-            traceback.print_exc()
+            count = count + 1
+            print("\r当前进度: {:.2f}%".format(count*100/len(lst)),end="")
             continue
-    
+ 
 def main():
-    stock_list_url = 'http://quote.eastmoney.com/sta'
+    stock_list_url = 'http://quote.eastmoney.com/stocklist.html'
+    stock_info_url = 'https://gupiao.baidu.com/stock/'
+    output_file = 'D:/BaiduStockInfo.txt'
+    slist=[]
+    getStockList(slist, stock_list_url)
+    getStockInfo(slist, stock_info_url, output_file)
     
     
 if __name__ == "__main__":
